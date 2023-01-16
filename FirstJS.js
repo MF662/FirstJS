e0904297a4489fa86ef201ad0c0e589761029439
Fatema Zohra Momin <mominfatema662@gmail.com>
2023-01-16 19:06:54 +0530

    3 Table

--- FirstJS.js
+++ FirstJS.js
@@ -3,3 +3,11 @@ console.log("Initial Code")
 var abc = ('Hello! World')
 
 console.log(abc)
+
+
+n = 3;
+for (let k = 1; k <= 10; k++) {
+    console.log(n*k);
+    
+}
+console.log('End of programme')
